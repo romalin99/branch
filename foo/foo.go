98c3597f8e729d07868f96f591bd806f733e53a8
romalin99 <romalin99@gmail.com>
2020-04-05 23:10:12 +0800

    test v2

--- foo/foo.go
+++ foo/foo.go
@@ -2,6 +2,6 @@ package foo
 
 import "fmt"
 
-func Foo() {
-	fmt.Println("foo.Foo of module: bitbucket.org/bigwhite/modules-major-branch v1.0.0")
+func Foo2() {
+	fmt.Println("foo.Foo2 of module: bitbucket.org/bigwhite/modules-major-branch v2.0.0")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/romalin99/branch
+module github.com/romalin99/branch/v2
 
 go 1.14
